a2980bf3bb373f790885ac8e7bdfea27116dfad4
XhmikosR <xhmikosr@gmail.com>
2026-04-12 14:59:42 +0300

    Refactor webpack alias resolution into separate functions

--- index.js
+++ index.js
@@ -18,48 +18,15 @@ const debug = debuglog('sass-lookup');
  * @param  {String|Array<String>} options.directory - the location(s) of all sass files
  * @return {String}
  */
-module.exports = function({
-  dependency,
-  filename,
-  directory,
-  webpackConfig
-} = {}) {
+module.exports = function({ dependency, filename, directory, webpackConfig } = {}) {
   if (dependency === undefined) throw new Error('dependency is not supplied');
   if (filename === undefined) throw new Error('filename is not supplied');
   if (directory === undefined) throw new Error('directory is not supplied');
 
   const WEBPACK_ALIAS_FLAG = '~';
   if (dependency.startsWith(WEBPACK_ALIAS_FLAG) && Boolean(webpackConfig)) {
-    dependency = dependency.replace(WEBPACK_ALIAS_FLAG, '');
-    let loadedConfig;
-
-    try {
-      webpackConfig = path.resolve(webpackConfig);
-      loadedConfig = require(webpackConfig);
-
-      if (typeof loadedConfig === 'function') {
-        loadedConfig = loadedConfig();
-      } else if (Array.isArray(loadedConfig)) {
-        loadedConfig = loadedConfig[0];
-      }
-    } catch (error) {
-      debug(`error loading the webpack config at ${webpackConfig}`);
-      debug(error.message);
-      debug(error.stack);
-      return '';
-    }
-
-    try {
-      const resolveConfig = { ...loadedConfig.resolve };
-      const resolver = webpackResolve.create.sync(resolveConfig);
-      const modulePath = resolver(process.cwd(), dependency);
-      return modulePath;
-    } catch (error) {
-      debug(`error resolving the webpack alias ${dependency}`);
-      debug(error.message);
-      debug(error.stack);
-      return '';
-    }
+    const webpackDependency = dependency.replace(WEBPACK_ALIAS_FLAG, '');
+    return resolveWebpackAliasDependency(webpackDependency, webpackConfig);
   }
 
   // Use the file's extension if necessary
@@ -103,6 +70,43 @@ module.exports = function({
     return path.resolve(directory, depDir, depName);
   }
 };
+
+function resolveWebpackAliasDependency(dependency, webpackConfig) {
+  let loadedConfig;
+  const resolvedConfigPath = path.resolve(webpackConfig);
+
+  try {
+    loadedConfig = loadWebpackConfig(resolvedConfigPath);
+  } catch (error) {
+    debug(`error loading the webpack config at ${resolvedConfigPath}`);
+    debug(error.message);
+    debug(error.stack);
+    return '';
+  }
+
+  try {
+    const resolveConfig = { ...loadedConfig.resolve };
+    const resolver = webpackResolve.create.sync(resolveConfig);
+    return resolver(process.cwd(), dependency);
+  } catch (error) {
+    debug(`error resolving the webpack alias ${dependency}`);
+    debug(error.message);
+    debug(error.stack);
+    return '';
+  }
+}
+
+function loadWebpackConfig(webpackConfigPath) {
+  let loadedConfig = require(webpackConfigPath);
+
+  if (typeof loadedConfig === 'function') {
+    loadedConfig = loadedConfig();
+  } else if (Array.isArray(loadedConfig)) {
+    loadedConfig = loadedConfig[0];
+  }
+
+  return loadedConfig;
+}
 
 function findDependency(searchDir, depName) {
   const nonPartialPath = path.resolve(searchDir, depName);
